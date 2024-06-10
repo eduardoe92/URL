0ff2e748c8b933545ffe58b456d4938f538c2beb
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-06-10 15:45:42 -0300

    nav act

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
+import LogoIcon from "../Logo/LogoIcon";
 
 function NavBar() {
   const [expand, setExpand] = useState(false);
@@ -24,27 +25,32 @@ function NavBar() {
   return (
     <nav
       className={`fixed w-full top-0 z-50 transition-all duration-300 ${
-        navColour ? "bg-[#1b1a2ea9] shadow-[0_10px_10px_0_rgba(9,5,29,0.171)] backdrop-blur-[15px]" : "bg-transparent"
+        navColour ? "bg-[black] shadow-[0_10px_10px_0_rgba(9,5,29,0.171)] backdrop-blur-[15px]" : "bg-black"
       }`}
     >
-      <div className="container mx-auto flex items-center justify-between p-[0.3rem_2rem] text-[1.2rem]">
-        <Link to="/" className="text-white text-2xl font-bold">
-          QR Create
-        </Link>
-        <button
-          className="relative bg-transparent border-none outline-none md:hidden"
-          onClick={() => setExpand(!expand)}
-        >
-          <span className="block bg-[#ffffff] h-[4px] w-[27px] mt-[5px] mb-[5px] rotate-0 left-0 opacity-100"></span>
-          <span className="block bg-[#ffffff] h-[4px] w-[27px] mt-[5px] mb-[5px] rotate-0 left-0 opacity-100"></span>
-          <span className="block bg-[#ffffff] h-[4px] w-[27px] mt-[5px] mb-[5px] rotate-0 left-0 opacity-100"></span>
-        </button>
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between p-[0.3rem_2rem] text-[1.2rem]">
+        <div className="flex justify-between w-full md:w-auto">
+          <Link to="/" className="flex items-center text-white text-2xl font-bold">
+            <LogoIcon/>
+            QR Create
+          </Link>
+          <button
+            className="md:hidden relative bg-transparent border-none outline-none"
+            onClick={() => setExpand(!expand)}
+          >
+            <span className="block bg-[#ffffff] h-[4px] w-[27px] mt-[5px] mb-[5px]"></span>
+            <span className="block bg-[#ffffff] h-[4px] w-[27px] mt-[5px] mb-[5px]"></span>
+            <span className="block bg-[#ffffff] h-[4px] w-[27px] mt-[5px] mb-[5px]"></span>
+          </button>
+        </div>
         <div
           className={`${
             expand ? "block" : "hidden"
-          } md:flex md:items-center w-full md:w-auto`}
+          } w-full md:flex md:items-center md:w-auto mt-4 md:mt-0`}
         >
-          <ul className="md:flex md:space-x-4 text-white">
+          <ul className={`md:flex md:space-x-4 text-white p-4 md:p-0 rounded-lg md:rounded-none ${
+            navColour || expand ? "bg-[#1b1a2ea9]" : "md:bg-black"
+          }`}>
             <li className="my-2 md:my-0">
               <Link to="/" onClick={() => setExpand(false)}>
                 <AiOutlineHome className="inline mr-1 mb-1" /> Home
