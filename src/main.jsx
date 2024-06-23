32932b1383d7b9d3bc522894a6ebf50744a718c9
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-06-23 01:29:10 -0300

    font

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from "./App";
+import '@fontsource-variable/jost';
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
