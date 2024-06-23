1ffa73bb749aef2a5020dc0c4dfc90a3e0b829f5
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-06-23 01:27:35 -0300

    tailwind css act

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '8xl': '110rem',
+      },
+      fontFamily: {
+        'sans': ['"Jost Variable"', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'background': '#192936',
+        'primary': '#333',
+        'secondary': '#ddd',
+        'tertiary': '#3b5a71',
+        'border':'#000000',
+      }
+    },
   },
   plugins: [],
 }
