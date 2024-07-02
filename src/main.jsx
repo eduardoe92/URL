827dcc0dd6c8bc4274df9e5e69bba8fce5cfe116
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-07-02 04:43:58 -0300

    dependencias

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
+import "./index.css";
 import App from "./App";
-import '@fontsource-variable/jost';
+import "@fontsource-variable/jost";
+import { NextUIProvider } from "@nextui-org/react";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <BrowserRouter>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </BrowserRouter>
 );
