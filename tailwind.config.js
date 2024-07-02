29c17e0e236a2b50caaec307cff41f0e40f3f8dd
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-07-02 04:21:47 -0300

    dependencias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from 'tailwindcss/colors';
+import formsPlugin from '@tailwindcss/forms';
+
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -11,16 +15,34 @@ export default {
         '18': '4.5rem',
       },
       fontFamily: {
-        'sans': 'var(--font-sans)',
+        sans: 'var(--font-sans)',
       },
       colors: {
-        'background': 'var(--color-background)',
-        'primary': 'var(--color-primary)',
-        'secondary': 'var(--color-secondary)',
-        'tertiary': 'var(--color-tertiary)',
-        'border': 'var(--color-border)',
-      }
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.trueGray,
+        indigo: colors.indigo,
+        red: colors.rose,
+        yellow: colors.amber,
+        pink: colors.pink,
+        purple: colors.purple,
+        blue: colors.blue,
+        green: colors.green,
+        background: 'var(--color-background)',
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        border: 'var(--color-border)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    formsPlugin,
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
 }
