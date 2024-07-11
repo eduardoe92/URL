343c5e0ae20518f3dd0f3df79f63620f870b6cb5
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-07-11 17:08:47 -0300

    Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,24 +1,5 @@
 import { useState, useEffect } from "react";
-import { FaLinkedin, FaInstagram, FaGithub, FaGlobe } from "react-icons/fa";
-
-const socialLinks = [
-  {
-    href: "https://eduardoeliaschacon-portfolio.vercel.app/",
-    label: "WebSite",
-    icon: FaGlobe,
-  },
-  {
-    href: "https://www.linkedin.com/in/eduardoe92/",
-    label: "Linkedin",
-    icon: FaLinkedin,
-  },
-  { href: "https://github.com/eduardoe92", label: "Github", icon: FaGithub },
-  {
-    href: "https://www.instagram.com/eduardo.e",
-    label: "Instagram",
-    icon: FaInstagram,
-  },
-];
+import { socialLinks } from "./SocialIcons";
 
 const Footer = () => {
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
@@ -31,12 +12,12 @@ const Footer = () => {
   }, [currentYear]);
 
   return (
-    <footer className="w-full relative max-w-screen-xl flex flex-col items-center justify-between mx-auto py-4 pb-8 pt-8 md:flex-row md:justify-between md:pt-16">
+    <footer className="relative flex flex-col items-center justify-between w-full max-w-screen-xl py-4 pt-8 pb-8 mx-auto md:flex-row md:justify-between md:pt-16">
       <hr className="absolute top-0 h-[2px] w-full min-w-[18rem] border-t-0 bg-transparent bg-gradient-to-r from-transparent via-white to-transparent bg-center md:my-9" />
 
-      <span className="flex flex-col gap-2 md:flex-row lg:gap-6 text-white text-center md:pl-16 lg:pl-6">
+      <span className="flex flex-col gap-2 text-center text-white md:flex-row lg:gap-6 md:pl-16 lg:pl-6">
         <strong>&copy; {currentYear}</strong>{" "}
-        <span aria-hidden className="font-semibold hidden md:inline">
+        <span aria-hidden className="hidden font-semibold md:inline">
           |
         </span>{" "}
         <br aria-hidden className="block md:hidden" />
@@ -48,14 +29,14 @@ const Footer = () => {
         className="my-8 h-[2px] w-full min-w-[18rem] border-t-0 bg-transparent bg-gradient-to-r from-transparent via-white to-transparent bg-center md:hidden"
       ></hr>
 
-      <ul className="flex flex-row gap-x-6 text-white items-center mt-0 md:pr-16 lg:pr-6">
+      <ul className="flex flex-row items-center mt-0 text-white gap-x-6 md:pr-16 lg:pr-6">
         {socialLinks.map(({ href, label, icon: Icon }) => (
           <li key={label}>
             <a
               href={href}
               target="_blank"
               rel="noopener noreferrer nofollow"
-              className="hover:scale-125 hover:opacity-70 transition inline-block"
+              className="inline-block transition hover:scale-125 hover:opacity-70"
             >
               <span className="sr-only">{label}</span>
               <Icon size={24} />
