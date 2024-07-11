e6dda318fa7f61b38dff72d0bb949538b0140af6
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-07-11 17:08:41 -0300

    Icons

---
+++ frontend/src/components/Footer/SocialIcons.jsx
@@ -0,0 +1,20 @@
+import { FaLinkedin, FaInstagram, FaGithub, FaGlobe } from "react-icons/fa";
+
+export const socialLinks = [
+  {
+    href: "https://eduardoeliaschacon-portfolio.vercel.app/",
+    label: "WebSite",
+    icon: FaGlobe,
+  },
+  {
+    href: "https://www.linkedin.com/in/eduardoe92/",
+    label: "Linkedin",
+    icon: FaLinkedin,
+  },
+  { href: "https://github.com/eduardoe92", label: "Github", icon: FaGithub },
+  {
+    href: "https://www.instagram.com/eduardo.e",
+    label: "Instagram",
+    icon: FaInstagram,
+  },
+];
