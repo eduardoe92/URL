1d6bac058b40509443538f0a51cf9f96c2efb7a6
eduardoe92 <eduardo.elias.1192@gmail.com>
2025-01-04 19:53:44 -0300

    deleted

--- backend/src/routes/qrRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createQRCode, getQRHistory } = require('../controllers/qrController');
-
-const router = express.Router();
-
-router.post('/create', createQRCode);
-router.get('/history', getQRHistory);
-
-module.exports = router;
