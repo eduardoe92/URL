cd4ad44329ab0fff363f0310a140c1cc413a8509
eduardoe92 <eduardo.elias.1192@gmail.com>
2025-01-16 21:52:35 -0300

    authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { register, login } = require('@/controllers/authController');
+const passport = require('passport');
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get(
+  '/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
 
 module.exports = router;
