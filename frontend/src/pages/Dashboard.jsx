b1222b4f763ffac2e837ae828598e069d68cf18d
eduardoe92 <eduardo.elias.1192@gmail.com>
2025-01-21 18:16:02 -0300

    firts commit

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import QRCodeGenerator from "../components/QRCodeGenerator";
+
+const Dashboard = () => {
+  const { user } = useContext(AuthContext);
+  const [qrCodes, setQrCodes] = useState([]);
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div>
+      <h1>Bienvenido, {user?.name}</h1>
+      <QRCodeGenerator />
+      <div className="mt-8">
+        {qrCodes.map((qr) => (
+          <img key={qr.id} src={qr.url} alt="Código QR" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
